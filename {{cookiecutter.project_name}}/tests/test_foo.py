c7828b013cae2d04d0aa0439d1d83c291e12b684
Raphael Vienne <raphael.vienne@live.fr>
2025-06-07 21:46:17 +0200

    fix jinja tempalte

--- {{cookiecutter.project_name}}/tests/test_foo.py
+++ {{cookiecutter.project_name}}/tests/test_foo.py
@@ -1,8 +1,7 @@
-{% if cookiecutter.layout == "src" %}
-from {{cookiecutter.project_name}}.main import main
-{% else %}
 {% if cookiecutter.layout == "backend" %}
 from {{cookiecutter.layout}}.main import main
+{% else %}
+from {{cookiecutter.project_name}}.main import main
 {% endif %}
 
 def test_main():
