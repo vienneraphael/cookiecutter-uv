f22ab56e9debaee063f6ad9577e5398332c171f4
Raphael Vienne <raphael.vienne@live.fr>
2025-06-07 21:33:54 +0200

    add relative tests to allow dry coverage run

--- {{cookiecutter.project_name}}/tests/test_foo.py
+++ {{cookiecutter.project_name}}/tests/test_foo.py
@@ -1,2 +1,9 @@
-def test_foo():
-    assert 2 + 7 == 9
+{% if {{cookiecutter.layout}} == "src" %}
+from {{cookiecutter.project_name}}.main import main
+{% else %}
+{% if {{cookiecutter.layout}} == "backend" %}
+from {{cookiecutter.layout}}.main import main
+{% endif %}
+
+def test_main():
+    assert main() == 42
--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/main.py
@@ -1,5 +1,5 @@
 def main():
-    pass
+    return 42
 
 
 if __name__ == "__main__":  # pragma: no cover
